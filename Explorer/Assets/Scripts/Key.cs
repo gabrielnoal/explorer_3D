84af6bd18633ac4e84057f78b5ff464c2bb5bff9
Gabriel Noal <gabriel.oliva@hotmail.com>
2021-05-09 19:08:23 -0300

    wip

---
+++ Explorer/Assets/Scripts/Key.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    
+    public Component Door;
+
+    void UnlockDoor(){
+        Door.GetComponent<BoxCollider>().enabled = true;
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (Input.GetKey(KeyCode.E))
+        {
+            UnlockDoor();
+            gameObject.SetActive(false);
+        }
+    }
+}
