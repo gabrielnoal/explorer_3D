9faec56e8e80b376a82933bd5555ff2203bce1da
WarlenRodrigues <warlen.cesarrodrigues@gmail.com>
2021-05-25 14:07:06 -0300

    Trying to show awesome inventory

---
+++ Explorer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public interface IInventoryItem
+{
+    string name { get; set; }
+    bool isInteractive { get; set; }
+    string imagePath { get; set; }
+}
+
+public class GameManager
+{
+
+    public List<IInventoryItem> inventoryItems;
+
+    private static GameManager _instance;
+
+    public static GameManager GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new GameManager();
+        }
+
+        return _instance;
+    }
+
+    private GameManager()
+    {
+        inventoryItems = new List<IInventoryItem>();
+    }
+
+
+}
