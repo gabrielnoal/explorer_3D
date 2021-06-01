06543428e32a00074a75eeecc8452b21a2f60a60
Gabriel Noal <gabriel.oliva@hotmail.com>
2021-06-01 13:04:22 -0300

    add KeypadDoor

---
+++ Explorer/Assets/Scripts/KeypadButton.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeypadButton : MonoBehaviour
+{
+    public GameObject keypad;
+    private float timestamp = 0f;
+    public float InputRate = 0.25f;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Time.time > timestamp)
+        {
+
+            if (Input.GetKey(KeyCode.Keypad0) || Input.GetKey(KeyCode.Alpha0))
+            {
+                ClickButton("0");
+            }
+            else if (Input.GetKey(KeyCode.Keypad1) || Input.GetKey(KeyCode.Alpha1))
+            {
+                ClickButton("1");
+            }
+            else if (Input.GetKey(KeyCode.Keypad2) || Input.GetKey(KeyCode.Alpha2))
+            {
+                ClickButton("2");
+            }
+            else if (Input.GetKey(KeyCode.Keypad3) || Input.GetKey(KeyCode.Alpha3))
+            {
+                ClickButton("3");
+            }
+            else if (Input.GetKey(KeyCode.Keypad4) || Input.GetKey(KeyCode.Alpha4))
+            {
+                ClickButton("4");
+            }
+            else if (Input.GetKey(KeyCode.Keypad5) || Input.GetKey(KeyCode.Alpha5))
+            {
+                ClickButton("5");
+            }
+            else if (Input.GetKey(KeyCode.Keypad6) || Input.GetKey(KeyCode.Alpha6))
+            {
+                ClickButton("6");
+            }
+            else if (Input.GetKey(KeyCode.Keypad7) || Input.GetKey(KeyCode.Alpha7))
+            {
+                ClickButton("7");
+            }
+            else if (Input.GetKey(KeyCode.Keypad8) || Input.GetKey(KeyCode.Alpha8))
+            {
+                ClickButton("8");
+            }
+            else if (Input.GetKey(KeyCode.Keypad9) || Input.GetKey(KeyCode.Alpha9))
+            {
+                ClickButton("9");
+            }
+            else if (Input.GetKey(KeyCode.Q))
+            {
+                ClickButton("Q");
+            }
+            else if (Input.GetKey(KeyCode.C))
+            {
+                ClickButton("C");
+            }
+        }
+    }
+
+    public void ClickButton(string value)
+    {
+        keypad.GetComponent<keypad>().ValueEntered(value);
+        gameObject.GetComponent<AudioSource>().Play();
+        timestamp = Time.time + InputRate;
+    }
+}
---
+++ Explorer/Assets/Scripts/keypad.cs
@@ -0,0 +1,146 @@
+﻿//333333333333333333333333333333333333333333333333333333333333333333\\
+//
+//          Arthur: Cato Parnell
+//          Description of script: control keypad button clicks and actions
+//          Any queries please go to Youtube: Cato Parnell and ask on video. 
+//          Thanks.
+//
+//33333333333333333333333333333333333333333333333333333333333333333\\
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class keypad : MonoBehaviour
+{
+    // *** CAN DELETE THESE ** \\
+    // Used to hide joystick and slider
+    [Header("Objects to Hide/Show")]
+    public GameObject objectToDisable;
+    public GameObject objectToDisable2;
+
+    // Object to be enabled is the keypad. This is needed
+    public GameObject objectToEnable;
+    public GameObject door;
+
+    // *** Breakdown of header(public) variables *** \\
+    // curPassword : Pasword to set. Ive set the password in the project. Note it can be any length and letters or numbers or sysmbols
+    // input: What is currently entered
+    // displayText : Text area on keypad the password entered gets displayed too.
+    // audioData : Play this sound when user enters in password incorrectly too many times
+
+    [Header("Keypad Settings")]
+    public string curPassword = "123";
+    public string input;
+    public Text displayText;
+    public AudioSource audioData;
+
+    //Local private variables
+    private bool keypadScreen;
+    private float btnClicked = 0;
+    private float numOfGuesses;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnClicked = 0; // No of times the button was clicked
+        numOfGuesses = curPassword.Length; // Set the password length.
+        audioData = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (btnClicked == numOfGuesses)
+        {
+            if (input == curPassword)
+            {
+                DisabeBoxCollider();
+                CloseKeypad();
+                OpenDoor();
+            }
+            else
+            {
+                ResetInput();
+                audioData.Play();
+            }
+
+        }
+    }
+
+    public void DisabeBoxCollider()
+    {
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+    }
+
+    public void OpenDoor()
+    {
+        door.GetComponent<OpenDoor>().UnlockDoor();
+        door.GetComponent<OpenDoor>()._OpenDoor();
+    }
+
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKey(KeyCode.E) && !keypadScreen)
+        {
+            keypadScreen = true;
+            objectToDisable.SetActive(false);
+            objectToDisable2.SetActive(false);
+            objectToEnable.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Disable sections when keypadScreen is set to true
+        if (Input.GetKey(KeyCode.E) && keypadScreen)
+        {
+            CloseKeypad();
+        }
+    }
+
+    public void ResetInput()
+    {
+        //Reset input varible
+        input = "";
+        displayText.text = input.ToString();
+        btnClicked = 0;
+    }
+
+    public void CloseKeypad()
+    {
+        objectToDisable.SetActive(true);
+        objectToDisable2.SetActive(true);
+        objectToEnable.SetActive(false);
+        keypadScreen = false;
+        ResetInput();
+    }
+
+    public void ValueEntered(string valueEntered)
+    {
+        switch (valueEntered)
+        {
+            case "Q": // QUIT
+                CloseKeypad();
+                break;
+
+            case "C": //CLEAR
+                input = "";
+                btnClicked = 0;// Clear Guess Count
+                displayText.text = input.ToString();
+                break;
+
+            default: // Buton clicked add a variable
+                btnClicked++; // Add a guess
+                input += valueEntered;
+                displayText.text = input.ToString();
+                break;
+        }
+
+
+    }
+}
