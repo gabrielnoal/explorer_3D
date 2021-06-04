b9f2291cd214b1a7eb021e2a1402691c06c950ba
WarlenRodrigues <warlen.cesarrodrigues@gmail.com>
2021-06-04 16:41:04 -0300

    Fixed inventory range

--- Explorer/Assets/Scripts/InventorySelector.cs
+++ Explorer/Assets/Scripts/InventorySelector.cs
@@ -39,7 +39,7 @@ public class InventorySelector : MonoBehaviour
         {
             if (gm.inventoryItems.Count == 0)
             {
-                for (int i = 0; i < 8; i++)
+                for (int i = 0; i < 6; i++)
                 {
                     GameObject itemImage = GameObject.Find("ItemImage");
 
@@ -74,7 +74,7 @@ public class InventorySelector : MonoBehaviour
 
         if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
         {
-            if (gm.selectedItem >= 7)
+            if (gm.selectedItem >= 5)
             {
                 gm.selectedItem = 0;
             }
